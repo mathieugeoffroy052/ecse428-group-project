3ed77685b4ee0b1f919cf73e0d79f4574e0a4309
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2022-02-08 23:30:16 -0500

    Implemented login_request method in views.py

--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -1,5 +1,10 @@
 from rest_framework.decorators import api_view
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 
 def public(request):
@@ -9,3 +14,25 @@ def public(request):
 @api_view(["GET"])
 def private(request):
     return HttpResponse("You should not see this message if not authenticated!")
+
+
+
+def login_request(request):
+    if(request.method == "POST"):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not NONE:
+                login(request, user)
+                messages.info(request, f"Successfully logged in as {username}")
+                return redirect("main:Home")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+          
+
+    form = AuthenticationForm()
+    return render(request, "main/login.html", {"form":form})
