23a153e8a7305a5b1cfdea5a4ee7473db010c803
SimonTheWang <simon.wang@mail.mcgill.ca>
2022-02-12 12:19:37 -0500

    correct response added

--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
 
 def public(request):
     return HttpResponse("You don't need to be authenticated to see this")
@@ -23,4 +25,4 @@ def sign_up(request):
     request = request.data
     user = User.objects.create_user(request["username"], request["email"], request["password"])
     user.save()
-    return HttpResponse("user created")
+    return Response({'success':'user created'}, status=status.HTTP_201_CREATED)
