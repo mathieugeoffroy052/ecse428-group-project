eeeac3f2d40566cf56fb58d4988b2fd575b2a229
SimonTheWang <simon.wang@mail.mcgill.ca>
2022-02-12 01:24:20 -0500

    added sign_up view

--- taskit-backend/tasklists/urls.py
+++ taskit-backend/tasklists/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tasklists.views import public, private
+from tasklists.views import public, private, sign_up
 
 
 urlpatterns = [
     path("public/", public),
     path("private/", private),
+    path("sign_up/", sign_up),
 ]
--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+import json
+from types import SimpleNamespace
 
 def public(request):
     return HttpResponse("You don't need to be authenticated to see this")
@@ -9,3 +11,22 @@ def public(request):
 @api_view(["GET"])
 def private(request):
     return HttpResponse("You should not see this message if not authenticated!")
+
+
+'''
+{
+	"username": "john1",
+	"email": "john@email.com",
+	"password": "johnpassword"
+}
+'''
+@api_view(["POST"])
+def sign_up(request):
+    parsed_request = request.data
+    user = User.objects.create_user(parsed_request["username"], parsed_request["email"], parsed_request["password"])
+    user.save()
+    return HttpResponse(
+        "username: " + user.username + "<br>"+
+        "email: " + user.email + "<br>" +
+        "password: " + user.password + "<br>"
+    )
