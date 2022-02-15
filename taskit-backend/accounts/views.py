097e0f1a5867b6535dffbfa7915775b061defc79
SimonTheWang <simon.wang@mail.mcgill.ca>
2022-02-14 23:33:21 -0500

    implemented user views using model

--- taskit-backend/accounts/views.py
+++ taskit-backend/accounts/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+from accounts.models import User
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import permission_classes
@@ -8,7 +8,6 @@ from rest_framework.permissions import AllowAny
 
 '''
 {
-	"username": "john1",
 	"email": "john@email.com",
 	"password": "johnpassword"
 }
@@ -17,6 +16,5 @@ from rest_framework.permissions import AllowAny
 @permission_classes([AllowAny])
 def sign_up(request):
     request = request.data
-    user = User.objects.create_user(request["username"], request["email"], request["password"])
-    user.save()
+    User.objects.create_user(request["email"], request["password"])
     return Response({'success':'user created'}, status=status.HTTP_201_CREATED)
--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -1,10 +1,5 @@
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from django.contrib.auth.models import User
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import AllowAny
 
 def public(request):
     return HttpResponse("You don't need to be authenticated to see this")
