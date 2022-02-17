ccce6db7f7af1df31b5a0f7445930e3fd64a7f82
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-02-17 13:35:27 -0500

    Define Task model

--- taskit-backend/tasklists/models.py
+++ taskit-backend/tasklists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Task(models.Model):
+    '''
+    Model for a user-defined task.
+    '''
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    due_datetime = models.DateTimeField()
+    estimated_duration = models.DurationField()
+    weight = models.IntegerField()
