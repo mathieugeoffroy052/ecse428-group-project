3a6b6f566911d14a9cb085d006ea5b27016108ae
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2022-02-16 19:40:07 -0500

    Resolving merge conflicts

--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -1,5 +1,10 @@
 from rest_framework.decorators import api_view
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, request
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 def public(request):
     return HttpResponse("You don't need to be authenticated to see this")
@@ -8,4 +13,5 @@ def public(request):
 @api_view(["GET"])
 def private(request):
     return HttpResponse("You should not see this message if not authenticated!")
+
 
