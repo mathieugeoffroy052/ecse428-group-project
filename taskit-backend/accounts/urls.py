7012c6f3e7950ed817cdefa2b28fd5dcc2f2415f
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2022-02-19 12:27:11 -0500

    Fix urls.py

--- taskit-backend/accounts/urls.py
+++ taskit-backend/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-import views
+from accounts.views import sign_up
+from accounts.views import login_request
+from accounts.views import logout_request
 
 urlpatterns = [
-    path("sign_up/", views.sign_up),
-    path("login/", views.login_request, name="login"),
-    path("logout/", views.logout_request, name="logout"),
+    path("sign_up/", sign_up),
+    path("login/", login_request, name="login"),
+    path("logout/", logout_request, name="logout"),
 ]
