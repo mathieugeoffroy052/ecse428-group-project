ce09c1ac98cb349652223d625cec07bcec57d236
amanij <amani.jammoul@mail.mcgill.ca>
2022-02-23 13:01:31 -0500

    add late flag and state to task model

--- taskit-backend/tasklists/models.py
+++ taskit-backend/tasklists/models.py
@@ -12,3 +12,16 @@ class Task(models.Model):
     due_datetime = models.DateTimeField(default=None, blank=True, null=True)
     estimated_duration = models.DurationField(default=None, blank=True, null=True)
     weight = models.IntegerField(default=None, blank=True, null=True)
+
+    late = models.BooleanField(default=None, null=True)
+    
+    NotStarted = 'NS'
+    InProgres = 'IP'
+    Completed = 'C'
+    TaskState = models.TextChoices(
+        (NotStarted, 'Not Started'),
+        (InProgres, 'In ProgresS'),
+        (Completed, 'Completed')
+    )
+    state = models.CharField(default=None, null=True, choices=TaskState, max_length=32)
+
