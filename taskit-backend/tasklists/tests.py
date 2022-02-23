d5ba4c9d397a7417ffe43ac63f3a4d63aa869f05
GabbyHalpin <gabriellehalpin@icloud.com>
2022-02-23 16:38:13 -0500

    correct test inputs

--- taskit-backend/tasklists/tests.py
+++ taskit-backend/tasklists/tests.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from django.test import TestCase
 from tasklists.models import Task
 from accounts.models import User
@@ -9,9 +10,9 @@ class TestClass(TestCase):
         user = User(email = "gingermuffin@gmail.com", password="GingerPower123")
         user.save()
 
-        task1 = Task(owner="gingermuffin@gmail.com", description = "eat food", due_datetime = '2022-02-06', estimated_duration = 30, weight = 50)
+        task1 = Task(owner=user, description = "eat food", due_datetime = datetime(2022,2,24,0,0,0,0), estimated_duration = timedelta(seconds=1800), weight = 50)
         task1.save()
-        task2 = Task(owner="gingermuffin@gmail.com", description = "drink all the milk", due_datetime = '2022-02-07', estimated_duration = 30, weight = 50)
+        task2 = Task(owner=user, description = "drink all the milk", due_datetime = datetime(2022,2,4,0,0,0,0), estimated_duration = timedelta(seconds=1800), weight = 50)
         task2.save()
 
         all_tasks = [task1,task2]
