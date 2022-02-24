19c1002fee37a97c6c6fa268a163b56c3bdd182c
SimonTheWang <simon.wang@mail.mcgill.ca>
2022-02-24 16:15:53 -0500

    might as well have clean code -> removed " =='' "

--- taskit-backend/accounts/views.py
+++ taskit-backend/accounts/views.py
@@ -16,9 +16,9 @@ from rest_framework.permissions import AllowAny
 @permission_classes([AllowAny])
 def sign_up(request):
     request = request.data
-    if "email" not in request or request["email"].strip() == "":
+    if "email" not in request or request["email"].strip():
         return Response({'No email address entered.'},  status=status.HTTP_400_BAD_REQUEST)
-    if "password" not in request or request["password"].strip() == "": 
+    if "password" not in request or request["password"].strip(): 
         return Response({'No password entered.'},  status=status.HTTP_400_BAD_REQUEST)
     if User.objects.filter(email=request["email"]):
         return Response({'This email address is already in use.'}, status=status.HTTP_409_CONFLICT)
