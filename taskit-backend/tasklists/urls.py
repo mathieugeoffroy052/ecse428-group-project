4b196d4af2c1b0d96bc750ee69511008033c275b
SimonTheWang <simon.wang@mail.mcgill.ca>
2022-02-23 22:12:19 -0500

    buggy implementation

--- taskit-backend/tasklists/urls.py
+++ taskit-backend/tasklists/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tasklists.views import public, private, task_list
+from tasklists.views import public, private, task_list, get_urgency
 
 urlpatterns = [
     path("public/", public),
     path("private/", private),
     path("tasks/", task_list),
+    path("get_urgency/", get_urgency),
 ]
--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from django.http import HttpResponse
 from tasklists.serializers import TaskSerializer
 from tasklists.models import Task
+from datetime import datetime, timedelta, date, timezone
+import math
 
 
 def public(request):
@@ -29,3 +31,18 @@ def task_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["POST"])
+def get_urgency(request) -> (bool, float):
+    request = request.data
+    task = Task.objects.get(pk=request)
+    task = TaskSerializer(task)
+    print(task)
+    remaining_timedelta = datetime.fromisoformat(task["due_datetime"].value) - datetime.now(timezone.utc)
+    remaining_hours = remaining_timedelta.days + remaining_timedelta.seconds / (24 * 60 * 60) # hours seems to be the most accurate depiction and most used case
+    t = datetime.strptime(task["estimated_duration"].value,"%d %H:%M:%S")#TODO FIX
+    delta = timedelta(days=t.day, hours=t.hour, minutes=t.minute, seconds=t.second)
+    estimated_hours = delta.days + delta.seconds / (24 * 60 * 60)
+    late = remaining_hours < 0
+    urgency = -1 * estimated_hours * remaining_hours if late else estimated_hours / remaining_hours # the later it is, the more urgent, and the sooner it is due, the more urgent
+    return Response({(late, math.atan(urgency) * 2/math.pi)}, status=status.HTTP_200_OK)
