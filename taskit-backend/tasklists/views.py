91773cc10614753a53ab03df74241a77c6d661df
Gabriel Lacroix <lacroixgabriel@gmail.com>
2022-02-23 20:41:43 -0500

    Add a task list and create view

--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -1,5 +1,9 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import HttpResponse
+from tasklists.serializers import TaskSerializer
+from tasklists.models import Task
 
 
 def public(request):
@@ -9,3 +13,19 @@ def public(request):
 @api_view(["GET"])
 def private(request):
     return HttpResponse("You should not see this message if not authenticated!")
+
+
+@api_view(["GET", "POST"])
+def task_list(request):
+    if request.method == "GET":
+        tasks = Task.objects.filter(owner=request.user)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = TaskSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
