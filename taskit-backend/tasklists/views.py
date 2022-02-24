9766638afa782eaca375916ebd32fe688ad902f8
amanij <amani.jammoul@mail.mcgill.ca>
2022-02-23 23:58:52 -0500

    add udpate view methods

--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -1,5 +1,9 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import HttpResponse
+from tasklists.serializers import TaskSerializer
+from tasklists.models import Task
 
 
 def public(request):
@@ -9,3 +13,44 @@ def public(request):
 @api_view(["GET"])
 def private(request):
     return HttpResponse("You should not see this message if not authenticated!")
+
+"""
+{
+	"pk": "52",
+	"late": True
+}
+"""
+
+@api_view(["POST"])
+def update_late_flag(request):
+    request = request.data
+    try:
+        t = Task.objects.get(pk=request["pk"])
+        s = TaskSerializer(t, data={'late': request["late"]}, partial=True)
+        if s.is_valid():
+            s.save()
+            return Response(s.data, status=status.HTTP_200_OK)
+        else:
+            return Response(s.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Task.DoesNotExist:
+        return Response('Exception: Data Not Found', status=status.HTTP_400_BAD_REQUEST)
+
+"""
+{
+	"pk": "52",
+	"state": "IP"
+}
+"""
+@api_view(["POST"])
+def update_state(request):
+    request = request.data
+    try:
+        t = Task.objects.get(pk=request["pk"])
+        s = TaskSerializer(t, data={'state': request["state"]}, partial=True)
+        if s.is_valid():
+            s.save()
+            return Response(s.data, status=status.HTTP_200_OK)
+        else:
+            return Response(s.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Task.DoesNotExist:
+        return Response('Exception: Data Not Found', status=status.HTTP_400_BAD_REQUEST)
