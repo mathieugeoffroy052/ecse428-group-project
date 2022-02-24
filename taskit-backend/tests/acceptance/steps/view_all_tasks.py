dbf16f672bb815475ba96f754bb34267ea830668
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2022-02-24 01:54:16 -0500

    Added acceptance tests

--- taskit-backend/tests/acceptance/steps/view_all_tasks.py
+++ taskit-backend/tests/acceptance/steps/view_all_tasks.py
@@ -1,9 +1,8 @@
 from accounts.models import User
 from tasklists.models import Task
-from accounts.views import login_request
 from behave import given, then, when
 from django.urls import reverse
-from hamcrest import assert_that, equal_to, not_none
+from hamcrest import assert_that, equal_to
 
 import optional
 
@@ -21,4 +20,23 @@ def step_impl(context):
         task = Task(owner=row['email'], due_datetime = row['due_date'], estimated_duration = row['estimated_duration'], weight = row['weight'])
         task.save()
 
+@given(u'"{email}" is logged in')
+def step_impl(context,email):
+    user = User.objects.filter(email=email)
+    password = user.password
+    client = context.test.client
+    client.login(email=email, password=password)
 
+@when(u'The user attempts to view all their tasks')
+def step_impl(context):
+    try:
+        context.response = context.client.post(reverse('view_all_tasks'))
+        print(f"Response: {context.response}")
+    except BaseException as e:
+        print(f"Exception: {e}")
+        context.error = e
+
+@then(u'the view function will return the tasks "{task_names}"')
+def step_impl(context, task_names):
+    user = User.objects.get(task_names=task_names)
+    assert_that(user.tasks, equal_to(task_names))
