977282a04b83d501ebe11d612c762ff1fd4c97d3
blissfulcat <howard.yu@mail.mcgill.ca>
2022-02-23 21:04:57 -0500

    something

--- taskit-backend/accounts/views.py
+++ taskit-backend/accounts/views.py
@@ -11,34 +11,37 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
-'''
+"""
 {
 	"email": "john@email.com",
 	"password": "johnpassword"
 }
-'''
+"""
+
+
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def sign_up(request):
     request = request.data
     User.objects.create_user(request["email"], request["password"])
-    return Response({'success':'user created'}, status=status.HTTP_201_CREATED)
-
+    return Response({"success": "user created"}, status=status.HTTP_201_CREATED)
 
 
 # Login upon request
-'''
+"""
 {
     "email": "john@smith.com",
     "password": "johnpassword"
 }
-'''
+"""
+
+
 def login_request(request):
-    if(request.method == "POST"):
+    if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
             user = authenticate(email=email, password=password)
             if user is not NoneType:
                 login(request, user)
@@ -48,16 +51,13 @@ def login_request(request):
                 messages.error(request, "Invalid email or password")
         else:
             messages.error(request, "Invalid email or password")
-          
 
     form = AuthenticationForm()
-    render(request, "main/login.html", {"form":form})
-    return Response({'success':'user created'}, status=status.HTTP_403_FORBIDDEN)
+    return render(request, "main/login.html", {"form": form})
 
 
 # Logout upon request
 def logout_request(request):
     logout(request)
     messages.inf(request, "Successfully logged out")
-    redirect("main:Home")
-    return Response({'success':'user logged out'}, status=status.HTTP_201_CREATED)
+    return redirect("main:Home")
--- taskit-backend/tasklists/urls.py
+++ taskit-backend/tasklists/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from tasklists.views import public, private
 
-urlpatterns = [
-    path("public/", public),
-    path("private/", private)
-]
+urlpatterns = [path("public/", public), path("private/", private)]
