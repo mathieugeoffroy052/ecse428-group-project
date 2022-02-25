fe15a133a50ad5888e06ed1675deb9b1ff266626
GabbyHalpin <gabriellehalpin@icloud.com>
2022-02-24 23:39:15 -0500

    created the test file

--- taskit-backend/tests/acceptance/steps/Ordering.py
+++ taskit-backend/tests/acceptance/steps/Ordering.py
@@ -56,52 +56,14 @@ def step_impl(context,email):
     # except BaseException as e:
     #     context.error = e
 
-@then(u'the task "{name}" shall exist in the system')
-def step_impl(context, name):
-    for task in Task.objects.all():
-        if task.name == name:
-            assert True
-    assert_that(context.response.status_code, equal_to(201))
-    assert_that(context.error, equal_to(None))
-
-
-@then(u'"{email}" shall have a task called "{name}" with due date "{due_date}", duration "{estimated_duration}", weight "{weight}", and state "Not started"')
-def step_impl(context,email,name,due_date,estimated_duration,weight):
+@then(u'Then the ordering will be "{order}"')
+def step_impl(context,order):
     request_data = {
-        'email': email if email != None else '',
-        'name': name if name != None else '',
-        'due_date': due_date if due_date != None else '',
-        'estimated_duration': estimated_duration if estimated_duration != None else '',
-        'weight': weight if weight != None else ''
+        'order': order if order != None else '',
     }
-    try:
-        #this does not exist yet, might have to change method name later
-        context.response = context.client.post(reverse('CreateTask_request'), request_data)
-        print(context.response)
-    except BaseException as e:
-        context.error = e
-
-@then(u'the number of tasks in the system shall be "5"')
-def step_impl(context):
-    number_of_tasks = 0
-    for task in Task.objects.all():
-        number_of_tasks += 1
-    assert number_of_tasks == 5
-
-
-
-@then(u'The message "Task created successfully." shall be displayed')
-def step_impl(context):
-    msg = context.message
-    assert_that(msg, equal_to("Task created successfully"))
-
-@then(u'no new task shall be created')
-def step_impl(context):
-    if context.response != None:
-        assert_that(context.response.status_code, not(equal_to(201)))
-
-@then(u'The error message "Log in to edit your tasks." shall be displayed')
-def step_impl(context):
-    e = context.error
-    assert_that(e, not_none())
-    assert_that(e.message, equal_to("Login in to edit your tasks."))
+    # try:
+    #     #this does not exist yet, might have to change method name later
+    #     context.response = context.client.post(reverse('CreateTask_request'), request_data)
+    #     print(context.response)
+    # except BaseException as e:
+    #     context.error = e
