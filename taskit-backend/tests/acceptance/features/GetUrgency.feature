1f00f14721161921c52b234bab4c80e13c82b8cf
GabbyHalpin <gabriellehalpin@icloud.com>
2022-02-25 13:00:08 -0500

    editing the acceptance test

--- taskit-backend/tests/acceptance/features/GetUrgency.feature
+++ taskit-backend/tests/acceptance/features/GetUrgency.feature
@@ -9,33 +9,15 @@ Feature: Calculate task Urgency
       | email                    | task_name                        | due_date   | estimated_duration | weight | state       |
       | obi-wan.kenobi@gar.gov   | Train Anakin                     | 2022-02-25 | 1576800            | 8      | Not started |
       | obi-wan.kenobi@gar.gov   | die                              | 2022-05-07 | 5                  | 7      | Not started |
-      | obi-wan.kenobi@gar.gov   | Train Luke                       | 2022-04-25 | 45                 | 5      | Not started |
+      | obi-wan.kenobi@gar.gov   | Train Luke                       | 2022-04-25 | 45                 | 2      | Not started |
 
   Scenario: Successfully order tasks (normal flow)
     Given "<email>" is logged in
-    When The user attempts to order their tasks by "Priority"
-    Then the ordering will be "Train Anakin, die, Train Luke"
+    When The user "<email>" attempts to order their tasks
+    Then the ordering by "Priority" will be "Train Anakin, die, Train Luke"
+    Then the ordering by "Importance" will be "Train Anakin, die, Train Luke"
+    Then the ordering by "Urgency" will be "Train Anakin, Train Luke, die"
     
     Examples:
       | email                        | password      |
       | obi-wan.kenobi@gar.gov       | jedimaster123 |
-  
-  Scenario: Successfully order tasks (alternate flow)
-    Given "<email>" is logged in
-    When The user attempts to order their tasks by "Importance"
-    Then the ordering will be "Train Anakin, die, Train Luke"
-
-    Examples:
-    | email                        | password      |
-    | obi-wan.kenobi@gar.gov       | jedimaster123 |
-
-
-  
-  Scenario: Successfully order tasks (alternate flow)
-    Given "<email>" is logged in
-    When The user attempts to order their tasks by "Urgency"
-    Then the ordering will be "Train Anakin, Train Luke, die"
-
-    Examples:
-    | email                        | password      |
-    | obi-wan.kenobi@gar.gov       | jedimaster123 |
--- taskit-backend/tests/acceptance/steps/Ordering.py
+++ taskit-backend/tests/acceptance/steps/Ordering.py
@@ -17,20 +17,11 @@ def step_impl(context):
         task = Task.objects.create_task(row['email'], row['task_name'],row['due_date'], row['estimated_duration'], row['weight'], row['state'])
         task.save()
 
-# @given(u' The following types of ordering exist')
-# def step_impl(context):
-#     for row in context.table:
-#         #no such thing as create_task, might have to change later
-#         task = Task.objects.create_task(row['email'], row['task_name'],row['due_date'], row['estimated_duration'], row['weight'], row['state'])
-#         task.save()
-
-
 #******************* need method name *************
-@when(u'The user "{email}" attempts to order their tasks by "Importance"')
+@when(u'The user "{email}" attempts to order their tasks')
 def step_impl(context,email):
     request_data = {
         'email': email if email != None else '',
-        'type': 'Importance'
     }
     # try:
         # #this does not exist yet, might have to change method name later
@@ -39,25 +30,36 @@ def step_impl(context,email):
     # except BaseException as e:
     #     context.error = e
 
-#******************* need method name *************
-@when(u'The user "{email}" attempts to order their tasks by "Urgency"')
-def step_impl(context,email):
-    request_data = {
-        'email': email if email != None else '',
-        'type': 'Urgency'
-    }
-    # try:
-        # #this does not exist yet, might have to change method name later
-        # context.response = context.client.post(reverse('CreateTask_request'), request_data)
-        # print(context.response)
-    # except BaseException as e:
-    #     context.error = e
-
-@then(u'Then the ordering will be "{order}"')
+@then(u'The ordering by "Priority" will be "{order}"')
 def step_impl(context,order):
-    request_data = {
-        'order': order if order != None else '',
-    }
+
+    order = order.split(", ")
+    #call view_all tasks 
+    # try:
+    #     #this does not exist yet, might have to change method name later
+    #     context.response = context.client.post(reverse('CreateTask_request'), request_data)
+    #     print(context.response)
+    # except BaseException as e:
+    #     context.error = e
+
+
+@then(u'The ordering by "Importance" will be "{order}"')
+def step_impl(context,order):
+
+    order = order.split(", ")
+    #call view_all tasks 
+    # try:
+    #     #this does not exist yet, might have to change method name later
+    #     context.response = context.client.post(reverse('CreateTask_request'), request_data)
+    #     print(context.response)
+    # except BaseException as e:
+    #     context.error = e
+
+@then(u'The ordering by "Urgency" will be "{order}"')
+def step_impl(context,order):
+
+    order = order.split(", ")
+    #call view_all tasks 
     # try:
     #     #this does not exist yet, might have to change method name later
     #     context.response = context.client.post(reverse('CreateTask_request'), request_data)
