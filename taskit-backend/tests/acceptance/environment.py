c42858566ca073d1d3e8ae896e7fadb7032355ad
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-02-26 15:34:17 -0500

    Update environment.py

--- taskit-backend/tests/acceptance/environment.py
+++ taskit-backend/tests/acceptance/environment.py
@@ -2,5 +2,11 @@
 behave environment module for acceptance testing taskit project
 """
 
+from django.test import Client
+
+def before_all(context):
+    context.client = Client()
+
 def before_scenario(context, _):
-    context.tasks = []
+    context.response = None
+    context.error = None
