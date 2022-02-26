d1e45e44487c2df3d218a288dcaafdebdfcb97b3
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-02-26 15:34:41 -0500

    Fix type errors in step definitions

--- taskit-backend/tests/acceptance/steps/Ordering.py
+++ taskit-backend/tests/acceptance/steps/Ordering.py
@@ -1,8 +1,8 @@
 from accounts.models import User
 from black import assert_equivalent
 from datetime import datetime, timedelta
+import json
 from tasklists.models import Task
-from tasklists.views import task_list
 from behave import *
 from django.urls import reverse
 from hamcrest import assert_that, equal_to, not_none
@@ -24,41 +24,49 @@ def step_impl(context):
 
 @given(u'"{email}" is logged in')
 def step_impl(context,email):
-    pass
+    user = User.objects.filter(email=email).first()
+    context.client.force_login(user)
 
-#******************* need method name *************
 @when(u'The user "{email}" attempts to order their tasks')
 def step_impl(context,email):
-    request_data = {
-        'email': email
-    }
-
-    context.tasks = task_list(request_data)
-    
+    try:
+        context.response = context.client.get(reverse('task_list'))
+        print(f"Response: {context.response}")
+        print(f"Response data: {json.loads(json.dumps(context.response.data))}")
+        for d in context.response.data:
+            print(f"Data: {d}")
+    except BaseException as e:
+        context.error = e
 
 @then(u'The ordering by "Priority" will be "{order}"')
 def step_impl(context,order):
-
+    assert_that(context.response, not_none())
+    assert_that(context.response.data, not_none())
     order = order.split(", ")
-    ordered_tasks = sorted(context.tasks, key=lambda t: t.priority) 
-    for x in range(len(context.tasks)):
+    tasks = json.loads(json.dumps(context.response.data))  # wtf
+    ordered_tasks = sorted(tasks, key=lambda t: float(t['priority']))
+    assert_that(len(ordered_tasks), equal_to(len(order)))
+    for x in range(len(ordered_tasks)):
         assert_that(ordered_tasks[x].description, equal_to(order[x]))
-
 
 @then(u'The ordering by "Importance" will be "{order}"')
 def step_impl(context,order):
-
+    assert_that(context.response, not_none())
+    assert_that(context.response.data, not_none())
     order = order.split(", ")
-    ordered_tasks = sorted(context.tasks, key=lambda t: t.importance)
-                
-    for x in range(len(context.tasks)):
+    tasks = json.loads(json.dumps(context.response.data))
+    ordered_tasks = sorted(tasks, key=lambda t: float(t['importance']))
+    assert_that(len(ordered_tasks), equal_to(len(order)))
+    for x in range(len(ordered_tasks)):
         assert_that(ordered_tasks[x].description, equal_to(order[x]))
 
 @then(u'The ordering by "Urgency" will be "{order}"')
 def step_impl(context,order):
-
+    assert_that(context.response, not_none())
+    assert_that(context.response.data, not_none())
     order = order.split(", ")
-    ordered_tasks = sorted(context.tasks, key=lambda t: t.urgency)             
-
-    for x in range(len(context.tasks)):
+    tasks = json.loads(json.dumps(context.response.data))
+    ordered_tasks = sorted(tasks, key=lambda t: float(t['urgency']))
+    assert_that(len(ordered_tasks), equal_to(len(order)))
+    for x in range(len(ordered_tasks)):
         assert_that(ordered_tasks[x].description, equal_to(order[x]))
