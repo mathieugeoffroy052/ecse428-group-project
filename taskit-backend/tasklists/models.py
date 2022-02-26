a9ce9c5684c7b677ee1221428f4104d3d1befb14
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-02-26 14:01:16 -0500

    Create TaskManager and create_task method

--- taskit-backend/tasklists/models.py
+++ taskit-backend/tasklists/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from accounts.models import User
 
+class TaskManager(models.Manager):
+    def create_task(self, owner, description, due_datetime, estimated_duration, weight):
+        task = self.create(owner=owner, description=description, due_datetime=due_datetime, estimated_duration=estimated_duration, weight=weight)
+        return task
 
 class Task(models.Model):
     """
     Model for a user-defined task.
     """
-
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
     due_datetime = models.DateTimeField(default=None, blank=True, null=True)
     estimated_duration = models.DurationField(default=None, blank=True, null=True)
     weight = models.IntegerField(default=None, blank=True, null=True)
+
+    objects = TaskManager()
