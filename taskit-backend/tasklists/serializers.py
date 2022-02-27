ffacaa5da66dd625b8d3b85e667af58384337f1d
David Hu <david.hu@mail.mcgill.ca>
2022-02-27 10:10:59 -0500

    changes to serializer to include state and id

--- taskit-backend/tasklists/serializers.py
+++ taskit-backend/tasklists/serializers.py
@@ -5,4 +5,4 @@ from tasklists.models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ("description", "due_datetime", "estimated_duration", "weight")
+        fields = ("description", "due_datetime", "estimated_duration", "weight", "state", "id")
