e1692b4dd7df9b6719e2a34961841577fe12a2bc
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2022-02-27 01:51:26 -0500

    Edited last step def

--- taskit-backend/tests/acceptance/environment.py
+++ taskit-backend/tests/acceptance/environment.py
@@ -1,3 +1,12 @@
 """
 behave environment module for acceptance testing taskit project
 """
+from django.test import Client
+
+def before_all(context):
+    context.client = Client()
+
+def before_scenario(context, _):
+    context.response = None
+    context.error = None
+    context.email = None
--- taskit-backend/tests/acceptance/steps/view_all_tasks.py
+++ taskit-backend/tests/acceptance/steps/view_all_tasks.py
@@ -3,7 +3,7 @@ from tasklists.models import Task
 from behave import given, then, when
 from django.urls import reverse
 from hamcrest import assert_that, equal_to
-
+import json
 import optional
 
 optional.init_opt_()
@@ -14,18 +14,12 @@ def step_impl(context):
         user = User.objects.create_user(row['username'], row['password'])
         user.save()
 
-@given(u'The following tasks exist')
-def step_impl(context):
-    for row in context.table:
-        task = Task(owner=row['email'], due_datetime = row['due_date'], estimated_duration = row['estimated_duration'], weight = row['weight'])
-        task.save()
-
 @given(u'"{email}" is logged in')
 def step_impl(context,email):
     user = User.objects.filter(email=email)
     password = user.password
-    client = context.test.client
-    client.login(email=email, password=password)
+    client = context.client
+    client.force_login(user)
 
 @when(u'The user attempts to view all their tasks')
 def step_impl(context):
@@ -38,5 +32,7 @@ def step_impl(context):
 
 @then(u'the view function will return the tasks "{task_names}"')
 def step_impl(context, task_names):
-    tasks = Task.objects.get(task_names=task_names) 
-    assert_that(tasks, equal_to(task_names))
+    tasks = context.response.data
+    for actual_task in tasks:
+        for expected_task in task_names:
+            assert_that(actual_task, equal_to(json.loads(expected_task)), 'Task list invalid')
