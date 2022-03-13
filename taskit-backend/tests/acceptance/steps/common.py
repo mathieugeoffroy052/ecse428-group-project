c9e7efc92b67db2d26a38707b80cafb960c22ec7
zoyamalhi <zoya.malhi@mail.mcgill.ca>
2022-03-13 16:05:38 -0400

    Fix some failing createTask tests

--- taskit-backend/tests/acceptance/steps/common.py
+++ taskit-backend/tests/acceptance/steps/common.py
@@ -64,13 +64,19 @@ def step_impl(context, message):
         message in msg, f"Expected response containing {message} but received {msg}"
     )
 
+# @then('The message "{message}" shall be displayed')
+# def step_impl(context, message):
+#     msg = context.response.data
+#     assert_that(msg, not_none())
+#     assert_that(
+#         message in msg, f"Expected response containing {message} but received {msg}"
+#     )
+
 @then('The message "{message}" shall be displayed')
 def step_impl(context, message):
-    msg = context.response.data
+    msg = context.response.data["success"]
     assert_that(msg, not_none())
-    assert_that(
-        message in msg, f"Expected response containing {message} but received {msg}"
-    )
+    assert_that(message in msg)
 
 
 @then("The user shall be at the login page")
