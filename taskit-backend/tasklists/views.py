4ad8ce837da457081507e16631619ad5e120d0c3
amanij <amani.jammoul@mail.mcgill.ca>
2022-03-27 02:19:46 -0400

    add post and delete task list view methods (not tested)

--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -4,8 +4,8 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
 from django.http import HttpResponse
-from tasklists.serializers import TaskSerializer
-from tasklists.models import Task
+from tasklists.serializers import TaskSerializer, TaskListSerializer
+from tasklists.models import Task, TaskList
 
 
 def public(request):
@@ -17,6 +17,7 @@ def private(request):
     return HttpResponse("You should not see this message if not authenticated!")
 
 
+# API to update task (state)
 @api_view(["PUT"])
 def update_state(request, pk):
     """
@@ -43,6 +44,7 @@ def update_state(request, pk):
         return Response("Exception: Data Not Found", status=status.HTTP_400_BAD_REQUEST)
 
 
+# APIs for Task
 @api_view(["GET", "POST", "DELETE"])
 def task_list(request):
     if request.method == "GET":
@@ -104,5 +106,55 @@ def remove_task(request):
         task = tasks.first()
         task.delete()
         return Response({"success": "Task deleted"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+# APIs for TaskList
+@api_view(["GET", "POST", "DELETE"])
+def task_list_list(request):
+    if request.method == "POST":
+        return post_task_list(request)
+    elif request.method == "DELETE":
+        return remove_task_list(request)
+    else:
+        return Response(
+            {"error": f"Invalid HTTP method {request.method}"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+def post_task_list(request):
+    """
+    POST:
+    {
+        "list_name":"School Work",
+    }
+    """
+    serializer = TaskListSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save(owner=request.user)
+        return Response(
+            {"data": serializer.data, "success": "Task list created succesfully."},
+            status=status.HTTP_201_CREATED,
+        )
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def remove_task_list(request):
+    """
+    DELETE:
+    {
+        "id": 123
+    }
+    """
+    id = request.data["id"]
+    taskLists = TaskList.objects.filter(id=id)
+    if taskLists:
+        taskList = taskList.first()
+        taskList.delete()
+        return Response({"success": "Task list deleted"}, status=status.HTTP_200_OK)
     else:
         return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
