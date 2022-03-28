16807a04583f4b4307127b705a238411b2b59c1a
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-03-28 12:42:16 -0400

    Rename view function

--- taskit-backend/tasklists/urls.py
+++ taskit-backend/tasklists/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from tasklists.views import public, private, update_state, edit_name, task_list
+from tasklists.views import public, private, update_state, edit_name, task
 
 urlpatterns = [
     path("public/", public),
     path("private/", private),
     path("update-state/<int:pk>", update_state, name="update_state"),
     path("edit-name/<int:pk>", edit_name, name="edit_name"),
-    path("tasks/", task_list, name="task"),
+    path("tasks/", task, name="task"),
 ]
