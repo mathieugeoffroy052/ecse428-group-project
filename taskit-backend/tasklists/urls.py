502ae30768896651298dc7c8778488cdfeb17b90
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-03-28 00:02:09 -0400

    Rename task-related endpoints

--- taskit-backend/tasklists/urls.py
+++ taskit-backend/tasklists/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("private/", private),
     path("update-state/<int:pk>", update_state, name="update_state"),
     path("edit-name/<int:pk>", edit_name, name="edit_name"),
-    path("tasks/", task_list, name="task_list"),
+    path("tasks/", task_list, name="task"),
 ]
