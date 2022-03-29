32490d4f44177435a03adcd6b4d11bf4f9c85e49
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-03-29 01:32:06 -0400

    Add explicit check for duplicate task list

--- taskit-backend/tasklists/views.py
+++ taskit-backend/tasklists/views.py
@@ -194,6 +194,10 @@ def post_task_list(request):
     serializer = TaskListSerializer(data=request.data)
 
     if serializer.is_valid():
+        # Check that the user doesn't already have a task list with the given name
+        new_list_name = request.data["list_name"]
+        if TaskList.objects.filter(owner=request.user, list_name=new_list_name).first():
+            return Response({"error": "This list name already exists."}, status=status.HTTP_409_CONFLICT)
         serializer.save(owner=request.user)
         return Response(
             {"data": serializer.data, "success": "Task list created succesfully."},
